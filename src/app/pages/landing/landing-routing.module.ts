c25d1a8b444c0fbf112d9c2cdcaee2d6eb6d3505
Ryan Tesoro <ryan.tesoro@lgusuite.com>
2022-10-04 07:51:25 +0800

    added routes for about-me, and portfolio

--- src/app/pages/landing/landing-routing.module.ts
+++ src/app/pages/landing/landing-routing.module.ts
@@ -2,7 +2,35 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { LandingComponent } from './landing.component';
 
-const routes: Routes = [{ path: '', component: LandingComponent }];
+const routes: Routes = [
+  {
+    path: '',
+    component: LandingComponent,
+    children: [
+      {
+        path: '',
+        pathMatch: 'full',
+        redirectTo: 'about-me',
+      },
+      {
+        path: 'about-me',
+        loadChildren: () =>
+          import('./components/about-me/about-me.module').then(
+            (m) => m.AboutMeModule
+          ),
+        data: { state: 'about' },
+      },
+      {
+        path: 'portfolio',
+        loadChildren: () =>
+          import('./components/portfolio/portfolio.module').then(
+            (m) => m.PortfolioModule
+          ),
+        data: { state: 'portfolio' },
+      },
+    ],
+  },
+];
 
 @NgModule({
   imports: [RouterModule.forChild(routes)],
