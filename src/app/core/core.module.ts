f58d15b59fbda358fc1d6a6ba9858209c196f633
Ryan Tesoro <ryan.tesoro@lgusuite.com>
2022-10-04 07:53:35 +0800

    updated core and shared module

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -2,13 +2,21 @@ import { NgModule } from '@angular/core';
 import { CommonModule } from '@angular/common';
 import { SharedModule } from '../shared/shared.module';
 import { LayoutComponent } from './components/layout/layout.component';
-import { FormsModule } from '@angular/forms';
 import { MaterialModule } from '../shared/module/material.module';
 import { SidebarComponent } from './components/sidebar/sidebar.component';
+import { FormsModule } from '@angular/forms';
+import { RouterModule } from '@angular/router';
+import { NavigationComponent } from './components/navigation/navigation.component';
 
 @NgModule({
-  declarations: [LayoutComponent, SidebarComponent],
-  imports: [CommonModule, SharedModule, FormsModule, MaterialModule],
+  declarations: [LayoutComponent, SidebarComponent, NavigationComponent],
+  imports: [
+    CommonModule,
+    FormsModule,
+    SharedModule,
+    MaterialModule,
+    RouterModule,
+  ],
   exports: [LayoutComponent, SidebarComponent],
 })
 export class CoreModule {}
--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -1,10 +1,24 @@
 import { NgModule } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { CommonModule } from '@angular/common';
 import { MaterialModule } from './module/material.module';
 import { FAwesomeModule } from './module/f-awesome.module';
 
+import { ButtonComponent } from './components/button/button.component';
+import { SocialIconComponent } from './components/social-icon/social-icon.component';
+
 @NgModule({
-  declarations: [],
-  imports: [MaterialModule, FAwesomeModule],
-  exports: [MaterialModule, FAwesomeModule],
+  declarations: [ButtonComponent, SocialIconComponent],
+  imports: [CommonModule, MaterialModule, FAwesomeModule, FormsModule],
+  exports: [
+    //modules
+    MaterialModule,
+    FAwesomeModule,
+    FormsModule,
+
+    //components
+    ButtonComponent,
+    SocialIconComponent,
+  ],
 })
 export class SharedModule {}
