917db660d5c8e448d47720b608d5dff47bbdca36
Ryan Tesoro <ryan.tesoro@lgusuite.com>
2022-10-04 07:52:52 +0800

    updated test file

--- src/app/core/components/layout/layout.component.spec.ts
+++ src/app/core/components/layout/layout.component.spec.ts
@@ -1,4 +1,5 @@
 import { ComponentFixture, TestBed } from '@angular/core/testing';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 import { LayoutComponent } from './layout.component';
 
@@ -8,9 +9,9 @@ describe('LayoutComponent', () => {
 
   beforeEach(async () => {
     await TestBed.configureTestingModule({
-      declarations: [ LayoutComponent ]
-    })
-    .compileComponents();
+      declarations: [LayoutComponent],
+      imports: [BrowserAnimationsModule],
+    }).compileComponents();
   });
 
   beforeEach(() => {
