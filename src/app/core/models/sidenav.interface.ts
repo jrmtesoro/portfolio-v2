c3c078efa5faf704a94f30ddb5a4936058dafd50
Ryan Tesoro <ryan.tesoro@lgusuite.com>
2022-10-04 07:52:33 +0800

    updated sidenav interface

--- src/app/core/models/sidenav.interface.ts
+++ src/app/core/models/sidenav.interface.ts
@@ -1,13 +1,7 @@
-import { MaterialFontAwesomeIcon } from 'src/app/shared/models/icon.interface';
-
 export interface SideNav {
   label: string;
   icon: string;
   tooltip: string;
   active?: boolean;
-}
-
-export interface SideNavSocial extends MaterialFontAwesomeIcon {
-  tooltip: string;
-  link: string;
+  route: string;
 }
